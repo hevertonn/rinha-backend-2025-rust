9c46cade53c77f32e9027510598998c1d5fc89e5
Heverton <hb.hevertonborges@gmail.com>
2025-07-24 11:53:55 -0300

    Added AppState to state of routes

--- src/routes.rs
+++ src/routes.rs
@@ -1,13 +1,14 @@
-use crate::handlers;
+use crate::{handlers, state::AppState};
 use axum::{
     Router,
     routing::{get, post},
 };
 
-pub fn init_routes() -> Router {
+pub fn init_routes(app_state: AppState) -> Router {
     let routes = Router::new()
         .route("/payments", post(handlers::payments))
-        .route("/payments-summary", get(handlers::payments_summary));
+        .route("/payments-summary", get(handlers::payments_summary))
+        .with_state(app_state);
 
     routes
 }
