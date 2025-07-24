303c9447218f9ece09394e18e91c64494524059d
Heverton <hb.hevertonborges@gmail.com>
2025-07-24 11:38:32 -0300

    Added payment processor

---
+++ src/payment_processor.rs
@@ -0,0 +1,16 @@
+use crate::payments_dto::PaymentDTO;
+use reqwest::Client;
+
+pub async fn post_payment(
+    payment_processor_url: String,
+    http_client: &Client,
+    payment: &PaymentDTO,
+) -> Result<(), reqwest::Error> {
+    http_client
+        .post(payment_processor_url)
+        .json(&payment)
+        .send()
+        .await?;
+
+    Ok(())
+}
