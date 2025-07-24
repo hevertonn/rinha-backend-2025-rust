3780bb4d2bd8c7d808e4209dc7a5655aa56d2296
Heverton <hb.hevertonborges@gmail.com>
2025-07-24 13:07:41 -0300

    Updated payments handler to redirect requests and added new dependencies in cargo.toml/lock

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,26 @@
-use crate::payments_dto::PaymentDTO;
-use axum::Json;
+use crate::{payment_processor, payments_dto::PaymentDTO, state::AppState};
+use axum::extract::{Json, State};
 
-pub async fn payments(Json(payload): Json<PaymentDTO>) {}
+pub async fn payments(State(app_state): State<AppState>, Json(payload): Json<PaymentDTO>) {
+    if let Err(e) = payment_processor::post(
+        &app_state.payment_processor_url.default,
+        &app_state.http_client,
+        &payload,
+    )
+    .await
+    {
+        println!("Payment processor default error: {}", e);
+
+        if let Err(e) = payment_processor::post(
+            &app_state.payment_processor_url.fallback,
+            &app_state.http_client,
+            &payload,
+        )
+        .await
+        {
+            println!("Payment processor fallback error: {}", e);
+        }
+    }
+}
 
 pub async fn payments_summary() {}
