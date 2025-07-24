cea967af1d00a0293c5b0bc3090d8802a0890597
Heverton <hb.hevertonborges@gmail.com>
2025-07-24 11:41:44 -0300

    Update main.rs: Send AppState to router

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+use state::AppState;
 use std::env;
 
 mod handlers;
+mod payment_processor;
 mod payments_dto;
 mod routes;
+mod state;
 
 #[tokio::main]
 async fn main() {
     let port = env::var("PORT").unwrap_or("8080".to_string());
 
-    let app = routes::init_routes();
+    let app_state = AppState::new();
+
+    let app = routes::init_routes(app_state);
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
