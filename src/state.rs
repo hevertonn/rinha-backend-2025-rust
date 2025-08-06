a7f547846f732a8d1dcfd22f68dd5a9ac2204e4e
Heverton <hb.hevertonborges@gmail.com>
2025-08-06 20:26:02 -0300

    Added redis connection pool

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,6 @@
-use reqwest::Client;
+use r2d2::Pool;
+use redis::Client;
+use reqwest::Client as ReqwestClient;
 use std::env;
 
 #[derive(Clone)]
@@ -10,17 +12,23 @@ pub struct PaymentProcessorsRoutes {
 #[derive(Clone)]
 pub struct AppState {
     pub payment_processors_routes: PaymentProcessorsRoutes,
-    pub http_client: Client,
+    pub http_client: ReqwestClient,
+    pub redis_connection_pool: Pool<Client>,
 }
 
 impl AppState {
     pub fn new() -> Self {
+        let client = Client::open(env::var("REDIS_URL").unwrap()).unwrap();
+
+        let pool = Pool::builder().build(client).unwrap();
+
         Self {
             payment_processors_routes: PaymentProcessorsRoutes {
                 default: env::var("PAYMENT_PROCESSOR_DEFAULT_URL").unwrap() + "/payments",
                 fallback: env::var("PAYMENT_PROCESSOR_FALLBACK_URL").unwrap() + "/payments",
             },
-            http_client: Client::new(),
+            http_client: ReqwestClient::new(),
+            redis_connection_pool: pool,
         }
     }
 }
